76c8af935676cd43f38cb1c78b8457bbceca71dd
mohitshaw1496-collab <mohitshaw1496@gmail.com>
2026-02-23 16:37:07 +0530

    Update BOT_TOKEN in vars.py
    Updated BOT_TOKEN to a new value.

--- vars.py
+++ vars.py
@@ -4,7 +4,8 @@ import os
 
 API_ID    = os.environ.get("API_ID", "24497690")
 API_HASH  = os.environ.get("API_HASH", "fcdf57fd911834f83aaf42f15fbcb200")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8368950488:AAEINxB32ug7YiqZuPm9a_ajGaxmuLVU-B4")
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
